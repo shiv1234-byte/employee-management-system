9b11e74651dacb12bb202f20d6fb96f4b25eab62
Shiv Rathor <rshiv8183@gmail.com>
2025-09-14 11:00:09 +0530

    Deploy manually to GitHub Pages

---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../../others/Header.jsx";
+ import TaskList from "../TaskList/TaskList.jsx";
+
+
+import TaskNumber from "../../others/TaskListNumbers";
+
+
+const EmployeeDashboard = (props) => {
+
+    return (
+      <div className='p-10 bg-[#1C1C1C] h-screen'>
+          
+          <Header changeUser={props.changeUser} data={props.data}/>
+          <TaskNumber data={props.data} />
+          <TaskList data={props.data} />
+      </div>
+    )
+  }
+  
+  export default EmployeeDashboard
