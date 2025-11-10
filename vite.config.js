562de30ff8900152aa50d17a1319b77860c83884
shiv1234-byte <139425771+shiv1234-byte@users.noreply.github.com>
2025-11-10 22:58:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-   base: "/employee-management-system/", 
+  base: "/employee-management-system/",
   plugins: [react(), tailwindcss()],
-})
+});
 
