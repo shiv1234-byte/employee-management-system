907d25cde576e47a293f38ad4cde53dd96aab91b
shiv1234-byte <139425771+shiv1234-byte@users.noreply.github.com>
2025-11-20 13:47:56 +0530

    Fix import casing for AuthProvider component

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
-import AuthProvider from './context/Authprovider.jsx';
+import AuthProvider from './context/AuthProvider.jsx';
 import logo from './assets/react.svg'; 
 
 
